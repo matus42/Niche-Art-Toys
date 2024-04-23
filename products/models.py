78de2d0446703ca134ddc2843d2e0386d48dd4fb
matus42 <134312272+matus42@users.noreply.github.com>
2024-04-23 21:18:48 +0100

    added sku to product model

--- products/models.py
+++ products/models.py
@@ -16,6 +16,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     is_sold = models.BooleanField(default=False)  # The flag for sold items 
