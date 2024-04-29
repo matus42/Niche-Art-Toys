b3e8ddf308d40c51d6978fd8fb2638ccb5099193
matus42 <134312272+matus42@users.noreply.github.com>
2024-04-29 21:30:23 +0100

    updated checkout template and views

--- Niche_art_toys/settings.py
+++ Niche_art_toys/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'checkout',
     
     # other
-    'crispy_forms'
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -83,11 +83,12 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
-            'buitins': [
+            'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
-                'crispy_forms.templatetags.crispy_forms_fields',
+                'crispy_forms.templatetags.crispy_forms_field',
             ]
         },
     },
