14a54a8001bf236ed9d7e7c5b6ee6f54634502ab
matus42 <134312272+matus42@users.noreply.github.com>
2024-05-02 20:56:25 +0100

    add product form for product admin

--- products/forms.py
+++ products/forms.py
@@ -1,7 +1,24 @@
 from django import forms
-from .models import Rating
+from .models import Rating, Product, Category
+
 
 class RatingForm(forms.ModelForm):
     class Meta:
         model = Rating
         fields = ['score', 'comment']
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-1'
